4b846357516a3c35b64707b5c1002b785ba29236
foysalkhan1 <foysal.hsbn@gmail.com>
2021-04-07 18:49:20 +0600

    shop coding done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,31 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
+    const total = cart.reduce((total, prd) => total + prd.price , 0)
+    let shipping = 0;
+    if (total > 35) {
+        shipping = 0;
+    } 
+    else if (total > 15) {
+        shipping = 4.99
+    }
+    else if (total > 15) {
+        shipping = 12.99
+    }
+    const tax = (total / 10).toFixed(2);
+    const grandTotal =(Number(tax) + total + shipping).toFixed(2);
+    const formatNumber = num => {
+        const precesion = num.toFixed(2)
+        return Number(precesion)
+    }
     return (
         <div>
             <h4>Order Summary</h4>
-            <p>Ordered Items: {cart.lenght}</p>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Product Price: $ {formatNumber(total)}</p>
+            <p><small>Shipping Cost: $ {shipping}</small></p>
+            <p>Tax + VAT: $ {tax}</p>
+            <p>Total Price: $ {grandTotal}</p>
         </div>
     );
 };
